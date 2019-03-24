0210f52d7c3f2f2d697554fd2d4abac38c533c9e
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-24 15:13:17 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,14 @@
+def instantiate_new_array
+my_new_array = []
+ my_new_array
+end
+
+def array_with_two_elements
+  my_two_array = ["brick", "mortar"]
+   my_two_array
+end
+
+def first_element
+  my_first_element = ["brick", "mortar", "wall", "house", "mansion"]
+  return first_element(array)[1]
+end
